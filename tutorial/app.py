12ae98301b2801bd292af65bd888963b78df76e3
Eugene Yoo <eugeneyoo@Eugenes-MacBook-Pro.local>
2023-01-21 20:03:09 +0900

    Tested URLs

--- tutorial/app.py
+++ tutorial/app.py
@@ -1,13 +1,34 @@
 import os
+import pyrebase
+from config import config
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+data = {"nae": "eugene"}
+db.child("uss").push(data)
+
 # a simple page that says hello
 @app.route('/')
 def hello():
     return render_template("index.html")
+
+@app.route('/eugene')
+def uploadList():
+  list={"food1":{"name":"apple", "taste":"5/10"}, "food2":"banana"}
+  db.child("foods").child("good food").set(list)
+  return "Done"
+
+@app.route('/read/food/<name>')
+def read_food(name):
+  users = db.child("foods").child("good food").child(name).get().val()
+  if (users is None):
+    return ("Empty!")
+  return (users)
 
 import auth, search
 app.register_blueprint(search.bp)
@@ -16,3 +37,4 @@ app.register_blueprint(auth.bp)
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
---
+++ tutorial/config.py
@@ -0,0 +1,6 @@
+config = {
+  "apiKey": "AIzaSyCsk0gSJCG_aMkwBsxNcogG4yXUGZ0b2Fg",
+  "authDomain": "projectId.firebaseapp.com",
+  "databaseURL": "https://uberforgrandparents-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  "storageBucket": "uberforgrandparents.appspot.com"
+}
